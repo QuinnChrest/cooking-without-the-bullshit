a8ad9fadf062e18c8e564f18cee7fe7339bcb923
Quinn Chrest <quinnchr@gmail.com>
2024-02-26 19:53:23 -0600

    svelte app for recipes

---
+++ recipe/src/routes/recipe/[id]/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { onMount } from "svelte";
+
+    /**
+   * @type {{ 
+   *    title: string;
+   *    author: string;
+   *    source: string;
+   *    pub_date: string;
+   *    description: string;
+   *    recipe: string;
+   * }}
+   */
+    let recipe;
+
+    onMount(async () => {
+        fetch("http://127.0.0.1:8000/recipes/" + data.recipe_id)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                recipe = data;
+            }).catch(error => {
+                console.log(error);
+            });
+    });
+
+    export let data;
+</script>
+
+<main>
+    {#if recipe != null}
+
+        <div>{recipe.title}</div>
+        <div>{recipe.author} - {recipe.pub_date}</div>
+        <div>{recipe.source}</div>
+        <div>{recipe.source}</div>
+    {/if}
+</main>
---
+++ recipe/src/routes/search/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { onMount } from "svelte";
+
+    /**
+   * @type {any[]}
+   */
+    let recipes = [];
+
+    onMount(async () => {
+        fetch("http://127.0.0.1:8000/recipes/")
+            .then(response => response.json())
+            .then(data => {
+                recipes = data;
+            }).catch(error => {
+                console.log(error);
+            });
+    });
+</script>
+
+<main>
+    <ul>
+        {#each recipes as recipe}
+            <li><a href="/recipe/{recipe.id}">{recipe.title}</a></li>
+        {/each}
+    </ul>
+</main>
