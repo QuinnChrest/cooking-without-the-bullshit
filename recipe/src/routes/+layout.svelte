1e424d3148e16dd0ddcfe336dfb085a6eb6591c9
Quinn Chrest <quinnchr@gmail.com>
2024-03-08 16:04:47 -0600

    minor

--- recipe/src/routes/+layout.svelte
+++ recipe/src/routes/+layout.svelte
@@ -2,13 +2,19 @@
     import "../app.pcss";
     import 'bootstrap/dist/css/bootstrap.min.css'
     import '@fontsource-variable/source-code-pro';
+    import swearingEmoji from "$lib/images/swearingEmoji.png";
 </script>
 
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-xl-7 col-xs-12">
             <div class="mb-4">
-                <a class="logo" href="/">🤬 COOKING WITHOUT THE BULLSH*T</a>
+                <a class="logo" href="/">
+                    <div class="d-flex align-items-center">
+                        <img src={swearingEmoji} alt="Swearing Emoji" height="40" width="40" class="me-2">
+                        COOKING WITHOUT THE BULLSH*T
+                    </div>
+                </a>
             </div>
             
             <slot></slot>
